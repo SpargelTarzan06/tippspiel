9222321a902cefc331cb3fe2a71f582130eafafd
SpargelTarzan06 <moritz.budde19@gmail.com>
2026-06-01 18:38:18 +0200

    Update page.tsx

--- app/admin/tipps-import/page.tsx
+++ app/admin/tipps-import/page.tsx
@@ -188,7 +188,7 @@ async function checkAdmin() {
 
       const startsWithMatchday = /^\d+\s/.test(cleanedLine)
 
-      let matchday = currentMatchday
+      let matchday: number | null = currentMatchday
       let textWithoutMatchday = cleanedLine
 
       if (startsWithMatchday) {
