027b4dab1e05d4aebcd76ecec23d9e1ad9bf5a5e
SpargelTarzan06 <moritz.budde19@gmail.com>
2026-06-04 08:47:28 +0200

    Short Team names

--- app/champions-league/page.tsx
+++ app/champions-league/page.tsx
@@ -602,6 +602,9 @@ function shortTeamName(name?: string) {
     'FC Augsburg': 'Augsburg',
     'Eintracht Frankfurt': 'Frankfurt',
     'TSG Hoffenheim': 'Hoffenheim',
+    'FC Schalke 04': 'Schalke',
+    'SV Elversberg': 'Elversberg',
+    'SC Paderborn 07': 'Paderborn',
   }
 
   return map[name] ?? name
--- app/page.tsx
+++ app/page.tsx
@@ -499,6 +499,9 @@ function getShortTeamName(name: string) {
     'FC Augsburg': 'Augsburg',
     'Eintracht Frankfurt': 'Frankfurt',
     'TSG Hoffenheim': 'Hoffenheim',
+    'FC Schalke 04': 'Schalke',
+    'SV Elversberg': 'Elversberg',
+    'SC Paderborn 07': 'Paderborn',
   }
 
   return map[name] ?? name
